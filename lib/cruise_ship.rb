4fec39324c4baf8742022a9d8c489d6f8e4d284e
Jacob W Jones <jacobwadejones76@gmail.com>
2020-11-05 18:38:26 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,11 +8,10 @@
 
 def select_winner(passengers)
   winner = ""
-  passengers.each do |key, value|
+  passengers.map do |key, value|
     if key == :suite_a && value.start_with?("A")
       winner = value
     end
   end
-  winner
 end
  
